1d8ae5586c518a67919ea0cae6f56b2c46a7369c
Evan Shockley <shockley.evan@gmail.com>
2021-08-05 11:34:13 -0700

    merge workflows

--- .github/scripts/update_context_wrapper.sh
+++ .github/scripts/update_context_wrapper.sh
@@ -2,13 +2,11 @@
 
 . /opt/XENONnT/setup.sh
 cd $HOME
-# get cutax versionf
+# get cutax version
 CUTAX_VERSION=$(grep "cutax_version=" create-env)
 CUTAX_VERSION=v${CUTAX_VERSION//cutax_version=}
 cd cutax
-echo "Checking out $CUTAX_VERSION"
 git checkout $CUTAX_VERSION
 python setup.py develop --user
 cd $HOME
-python -c "import cutax; print(cutax.__version__)"
-# python .github/scripts/update-context-collection.py
+python .github/scripts/update-context-collection.py
