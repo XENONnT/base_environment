5d423108e4889aa17beb238185b4873244d5bdb2
Evan Shockley <shockley.evan@gmail.com>
2021-10-18 15:12:27 -0700

    Update testing

--- .github/scripts/create-utilix-config.sh
+++ .github/scripts/create-utilix-config.sh
@@ -6,13 +6,13 @@ cat > $HOME/.xenon_config <<EOF
 rundb_api_url = $RUNDB_API_URL
 rundb_api_user = $RUNDB_API_USER
 rundb_api_password = $RUNDB_API_PASSWORD
-pymongo_url = $PYMONGO_URL
-pymongo_user = $PYMONGO_USER
-pymongo_password = $PYMONGO_PASSWORD
-pymongo_database = $PYMONGO_DATABASE
 xent_url = $PYMONGO_URL
 xent_user = $PYMONGO_USER
 xent_password = $PYMONGO_PASSWORD
 xent_database = $PYMONGO_DATABASE
+xe1t_url = $XE1T_URL
+xe1t_user = $XE1T_USER
+xe1t_password = $XE1T_PASSWORD
+xe1t_database = $XE1T_DATABASE
 EOF
 
