6661168725f2e529e34c79571bdc7a4386ff99d3
Evan Shockley <shockley.evan@gmail.com>
2022-03-03 05:55:31 -0800

    Another try

--- .github/scripts/update_context_wrapper.sh
+++ .github/scripts/update_context_wrapper.sh
@@ -4,6 +4,7 @@
 cd $HOME
 
 echo "ref name: ${GITHUB_REF_NAME}"
+echo "ref type: ${GITHUB_REF_TYPE}"
 
 # get cutax version
 CUTAX_VERSION=$(grep "cutax_version=" create-env)
@@ -13,4 +14,4 @@ cd cutax
 git checkout $CUTAX_VERSION
 python setup.py install --user
 cd $HOME
-python .github/scripts/update-context-collection.py ${GITHUB_REF_NAME}
+python .github/scripts/update-context-collection.py ${GITHUB_REF_NAME} ${GITHUB_REF_TYPE}
