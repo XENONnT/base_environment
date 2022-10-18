314bce3797af8a9047ce4deef1691066dda67a71
Joran Angevaare <j.angevaare@nikhef.nl>
2022-10-18 13:01:48 +0200

    split and try

--- .github/scripts/do_all_tests.sh
+++ .github/scripts/do_all_tests.sh
@@ -2,8 +2,15 @@
 . /opt/XENONnT/setup.sh
 cd $HOME
 
+echo 'pwd' `pwd`
+echo 'ls' `ls`
+echo '/home' `ls /home`
+echo '/home/.github' `ls /home/.github`
+echo '$HOME' $HOME
+
+# FIXME add XEDOCS
 for package in strax straxen wfsim pema cutax admix gfal;
 do
   echo testing $package;
-  bash /home/.github/scripts/do_tests.sh $package;
+  bash .github/scripts/do_tests.sh $package;
 done
