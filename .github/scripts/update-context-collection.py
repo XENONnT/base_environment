b4768f78364f12003b38f2e4e2a8901bf5e98453
Yossi Mosbacher <joe.mosbacher@gmail.com>
2023-07-02 11:35:54 +0300

    move context rename to after creating it

--- .github/scripts/update-context-collection.py
+++ .github/scripts/update-context-collection.py
@@ -32,8 +32,7 @@ def main():
     for context in context_list:
         # pass cuts_for=None so that we don't track the cut lineages. They aren't saved anyway
         # skip contexts that raise errors
-        if context == 'xenonnt_offline':
-            context = f'xenonnt_{cutax.contexts.DEFAULT_XEDOCS_VERSION}'
+        
         try:
             st = getattr(cutax.contexts, context)(cuts_for=None,
                                                   _include_rucio_remote=True)
@@ -43,6 +42,8 @@ def main():
 
         hash_dict = {dtype: dtype_info['hash'] for dtype, dtype_info in st.provided_dtypes().items()}
 
+        if context == 'xenonnt_offline':
+            context = f'xenonnt_{cutax.contexts.DEFAULT_XEDOCS_VERSION}'
         doc = dict(name=context,
                    date_added=datetime.datetime.utcnow(),
                    hashes=hash_dict,
