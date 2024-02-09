0596fe417f2dbc1614fb455d81d2174cc0fad87b
Dacheng Xu <dx2227@columbia.edu>
2024-02-09 22:43:03 +0800

    Merge `ax_env` into `base_environment` (#1487)
    * Should not use `straxen.get_resource` because the text file is not static
    
    * Add extra_requirements/requirements-tests.txt
    
    * Add duplication detection
    
    * Minor change
    
    * Fix bug
    
    * Minor ddebug
    
    * Minor debug
    
    * Print in different line
    
    * Debug
    
    * Test
    
    * Test
    
    * Debug
    
    * Remove duplications
    
    * Add setup.py
    
    ---------
    
    Co-authored-by: Daniel Wenz <43881800+WenzDaniel@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+def open_requirements(path):
+    with open(path) as f:
+        requires = [
+            r.split('/')[-1] if r.startswith('git+') else r
+            for r in f.read().splitlines()]
+    return requires
+
+with open('README.md') as file:
+    readme = file.read()
+
+requires = open_requirements('extra_requirements/requirements-tests.txt')
+
+setuptools.setup(
+    name='base_environment',
+    version='2024.01.1',
+    description='Base software environment for XENONnT, including python and data management tools',
+    author='base_environment contributors, the XENON collaboration',
+    url='https://github.com/XENONnT/base_environment',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    setup_requires=['pytest-runner'],
+    install_requires=requires,
+    python_requires=">=3.9",
+    packages=setuptools.find_packages() + ['extra_requirements'],
+    package_dir={'extra_requirements': 'extra_requirements'},
+    package_data={'extra_requirements': ['requirements-tests.txt']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    zip_safe=False,
+)
