c2cd5ead1b5f65ba1013820f8014fcab488a597f
Dacheng Xu <dx2227@columbia.edu>
2025-02-04 00:53:30 -0500

    Prepare for 2025.02.1 (#2001)
    * Prepare for 2025.02.1
    
    * Update pandas
    
    * Debug keras
    
    * Lower numpy
    
    * Lower numpy
    
    * Lower ml-dtypes
    
    * Update appletree and alea
    
    * Update appletree and alea
    
    * Install fsspec
    
    * lower fsspec
    
    * Update version of rframe and xedocs
    
    * Update rframe
    
    * Downgrade zarr
    
    ---------
    
    Co-authored-by: Husheng Guan <guan81@purdue.edu>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = open_requirements('extra_requirements/requirements-tests.txt')
 
 setuptools.setup(
     name='base_environment',
-    version='2025.01.4',
+    version='2025.02.1',
     description='Base software environment for XENONnT, including python and data management tools',
     author='base_environment contributors, the XENON collaboration',
     url='https://github.com/XENONnT/base_environment',
