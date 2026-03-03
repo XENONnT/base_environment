a0987c81daeb8e7e4262ed1f56ec435a077d5996
dachengx <dx2227@columbia.edu>
2026-03-03 04:16:06 -0600

    Update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = open_requirements('extra_requirements/requirements-tests.txt')
 
 setuptools.setup(
     name='base_environment',
-    version='2026.03.1',
+    version='sr2_cevns_unblind',
     description='Base software environment for XENONnT, including python and data management tools',
     author='base_environment contributors, the XENON collaboration',
     url='https://github.com/XENONnT/base_environment',
