8a8ad953cf148c2e9925c5bbd579dd77a4f3636f
Caio Takanori Oba Ishikawa <caioishikawa@midway3-login3.rcc.local>
2026-04-20 02:44:40 -0500

    testing with cutax py311_to_py312 branch

--- .github/scripts/do_tests.sh
+++ .github/scripts/do_tests.sh
@@ -53,7 +53,7 @@ case "$1" in
 
     CUTAX_VERSION=$(grep "cutax_version=" create-env)
     CUTAX_VERSION=${CUTAX_VERSION//cutax_version=}
-    CUTAX_VERSION=test_pre_sr3
+    CUTAX_VERSION=py311_to_py312
     echo "Testing with cutax version ${CUTAX_VERSION}"
     cd cutax
     if [ $CUTAX_VERSION != 'latest' ]
