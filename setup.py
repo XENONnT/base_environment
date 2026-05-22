49122d49cf1ffbae0d5168ef97546c31b9bf36a0
Caio Takanori Oba Ishikawa <caioishikawa@midway3-login3.rcc.local>
2026-05-22 04:37:17 -0500

    update to 2026.05.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = open_requirements('extra_requirements/requirements-tests.txt')
 
 setuptools.setup(
     name='base_environment',
-    version='2026.05.1',
+    version='2026.05.2',
     description='Base software environment for XENONnT, including Python, ROOT, Geant4 and data management tools',
     author='base_environment contributors, the XENON collaboration',
     url='https://github.com/XENONnT/base_environment',
